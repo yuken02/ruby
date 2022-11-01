b26ed7e38a2b35e0ca6a8b06f8843ecfc35e86e4
yuken02 <git@github.com:yuken02/practice.git>
2022-11-01 11:15:28 +0000

    [Finish]2-6 exelcise6-2

---
+++ lesson6-2.rb
@@ -0,0 +1,24 @@
+puts "計算を始めます"
+puts "何回計算を繰り返しますか？"
+
+rep = gets.to_i
+i = 1
+
+while rep>=i do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+  puts "計算結果を出力します"
+  puts "#{a}+#{b}=#{a+b}"
+  puts "#{a}-#{b}=#{a-b}"
+  puts "#{a}*#{b}=#{a*b}"
+  puts "#{a}/#{b}=#{a/b}"
+
+  i += 1
+end
+puts "計算を終了します"
